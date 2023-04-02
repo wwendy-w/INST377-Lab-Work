9824b8410155b9d130a7de28c2a90f730053fe2e
Wendy Wu <wendywu3110@gmail.com>
2023-04-01 23:44:38 -0400

    Initial Commit - Lab7

---
+++ client/lab_7/script.js
@@ -0,0 +1,103 @@
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+function getRandomIntInclusive(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+  function injectHTML(list) {
+    console.log('fired injectHTML')
+    const target = document.querySelector('#restaurant_list');
+    target.innerHTML = '';
+    list.forEach((item) => {
+      const str = `<li>${item.name}</li>`;
+      target.innerHTML += str
+  
+    })
+  }
+  
+  function filterList(list, query) {
+    return list.filter((item) => {
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = query.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    })
+  }
+  
+  function cutRestaurantList(list){
+    console.log('fired cut list');
+    const range = [...Array(15).keys()];
+    return newArray = range.map((item) => {
+      const index = getRandomIntInclusive(0, list.length - 1);
+      return list[index]
+    })
+  }
+  
+  async function mainEvent() { // the async keyword means we can make API requests
+    const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+    const filterButton = document.querySelector('#filter_button');
+    const loadDataButton = document.querySelector('#data_load');
+    const generateListButton = document.querySelector('#generate');
+    
+    const loadAnimation = document.querySelector('#data_load_animation');
+    loadAnimation.style.display = 'none';
+    
+    let currentList = []; 
+  
+    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+      console.log('Loading data'); // this is substituting for a "breakpoint"
+      loadAnimation.style.display = 'inline-block';
+  
+      const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+      /*
+     ## Get request with query parameters
+  
+        const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
+  
+        The above request uses "string interpolation" to include an encoded version of your form values
+        It works because it has a ? in the string
+        Replace line 37 with it, and try it with a / instead to see what your server console says
+  
+        You can check what you sent to your server in your GET request
+        By opening the "network" tab in your browser developer tools and looking at the "name" column
+        This will also show you how long it takes a request to resolve
+      */
+  
+      // This changes the response from the GET into data we can use - an "object"
+      currentList = await results.json();
+      loadAnimation.style.display = 'none';
+      console.table(currentList); 
+    });
+  
+    filterButton.addEventListener('click', (event) => {
+      console.log('clicked FilterButton');
+  
+      const formData = new FormData(mainForm);
+      const formProps = Object.fromEntries(formData);
+  
+      console.log(formProps);
+      const newList = filterList(currentList, formProps.resto);
+  
+      console.log(newList);
+      injectHTML(newList);
+  })
+  
+  generateListButton.addEventListener('click', (event) => {
+    console.log('generate new list');
+    const restaurantsList = cutRestaurantList(currentList);
+    injectHTML(restaurantsList);
+  })
+  
+  
+  }
+  
+  /*
+    This adds an event listener that fires our main event only once our page elements have loaded
+    The use of the async keyword means we can "await" events before continuing in our scripts
+    In this case, we load some data when the form has submitted
+  */
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  
